437ec85b057a2183c0aaad5e0407b197d2c5bf8a
gbxdl <daas.leer@gmail.com>
2020-03-15 22:08:16 +0100

    wrote a bot that always play defense at random distance

---
+++ classes/defenseBot.py
@@ -0,0 +1,35 @@
+from classes.gameState import *
+import random
+
+class defenseBot:
+    
+    def __init__(self):
+        pass
+        
+    def makeMove(self,gameState):
+        moveOptions = []
+        if gameState.lastMove:
+            if gameState.onMove == 1:
+                moveOptions = [(gameState.lastMove[0] + y, gameState.lastMove[1] - x) for y in [0,1] for x in [1,2,3]] #move above somewhat random
+            else:
+                moveOptions = [(gameState.lastMove[0] + y, gameState.lastMove[1] - x) for y in [1,2,3] for x in [0,1]]#move left somewhat random
+                
+        moveOptions = [x for x in gameState.possibleMoves() if x in moveOptions] #take overlap
+        
+        if moveOptions:
+            move = random.choice(moveOptions)
+            return move
+        
+        if gameState.lastMove:
+            if gameState.onMove == 1:
+                moveOptions = [(gameState.lastMove[0] + y, gameState.lastMove[1] + x) for y in [0,1] for x in [1,2,3]] #move above somewhat random
+            else:
+                moveOptions = [(gameState.lastMove[0] + y, gameState.lastMove[1] - x) for y in [1,2,3] for x in [0,1]]#move left somewhat random
+        
+        moveOptions = [x for x in gameState.possibleMoves() if x in moveOptions] #take overlap
+        
+        if moveOptions:
+            move = random.choice(moveOptions)
+            return move
+        
+        return random.choice(list(gameState.possibleMoves()))
