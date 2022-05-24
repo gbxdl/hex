c03e3ea23f98e21e596d2a80156f6cdd7866d1ab
Daan <daas.leer@gmail.com>
2022-05-24 10:19:29 +0200

    changes representation of hexagon to list of list
    from list of tuples so that javascript can handle it

--- classes/gui_base.py
+++ classes/gui_base.py
@@ -17,12 +17,12 @@ class gui_base:
         y = row * (math.sqrt(3) / 2 * self.width) + self.fromTheEdge
         r = self.width / 2
         R = r * 2 / math.sqrt(3)
-        top = (x, y - R)
-        bottom = (x, y + R)
-        topLeft = (x - r, y - R / 2)
-        bottomLeft = (x - r, y + R / 2)
-        topRight = (x + r, y - R / 2)
-        bottomRight = (x + r, y + R / 2)
+        top = [x, y - R]
+        bottom = [x, y + R]
+        topLeft = [x - r, y - R / 2]
+        bottomLeft = [x - r, y + R / 2]
+        topRight = [x + r, y - R / 2]
+        bottomRight = [x + r, y + R / 2]
         return [top, topRight, bottomRight, bottom, bottomLeft, topLeft]
 
     def update(self):
